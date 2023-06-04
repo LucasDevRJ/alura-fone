5022581b3e7d7aa2bd4f88fcadf11b90a6711267
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-06-04 20:26:27 -0300

    Lógica para imprimir números clicados no telefone

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
-let listaDeTeclados = document.querySelectorAll(".principal__teclado__botao");
+let listaDeBotoes = document.querySelectorAll(".principal__teclado__botao");
+let entradaDoTelefone = document.querySelector(".principal__entrada");
 
-console.log(listaDeTeclados);
+function imprimiValorNoCampoTelefone(idTeclado) {
+	entradaDoTelefone.value += idTeclado;
+}
+
+for (let i = 0; i < listaDeBotoes.length; i++) {
+	let botao = listaDeBotoes[i];
+	let botaoValor = botao.value;
+	botao.onclick = function() {
+		imprimiValorNoCampoTelefone(botaoValor);
+	}
+}
